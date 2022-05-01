b06b5af2aa3e1b7781667367933170b62f723314
kmakmichael <m_kmak@u.pacific.edu>
2022-05-01 14:59:05 -0700

    btcomms: add confirm()

--- btcomms.py
+++ btcomms.py
@@ -16,6 +16,9 @@ class BTComms:
             print(f'error connecting to {self.target}')
             sys.exit(1)
 
+    def confirm():
+        send('R')
+
 
     # send a string
     def send(self, msg):
